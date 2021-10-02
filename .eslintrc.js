d53dfe2d57620308ec56cc8eb8bf15d81428c780
Maurizio Paletti <maurizio.paletti@fasttrack-solutions.com>
2021-10-02 15:20:04 +0200

    🎉 first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  rules: {
+    'vue/require-default-prop': 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="less">
+body {
+  margin: 0;
+}
+
+div[id*='@ft/backoffice-login'] {
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  background: #2a2a31;
+  color: #fff;
+
+  * {
+    font-family: 'Inter', sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+  }
+}
+
+.grecaptcha-badge {
+  visibility: hidden;
+}
+</style>
